0a8fecadda567be60337af8c35f218b70a79d53a
prithvivarma-nallapuraju <nvpprithvivarma@gmail.com>
2023-06-07 11:31:06 +0530

    updated base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Loading from './components/Loading'
-import { useState, useEffect } from 'react'
-
 
 function App() {
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 5000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Loading loading={loading} />
-      <div className={`main-page ${loading ? "fade-out" : ""}`}>
-        <h1>Welcome to my app!</h1>
-        <p>This is the main page.</p>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
